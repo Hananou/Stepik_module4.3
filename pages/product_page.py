3e8dc4a9628804f716a154d00d37c3aa8b1e46d4
Hananou <hananoulutin@gmail.com>
2019-09-29 19:49:29 +0300

    updated product page asserts

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,5 +4,9 @@ import math
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
-        add_to_busket_link = self.browser.find_element(*ProductPageLocators.PRODUCT_LINK)
+        add_to_busket_link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_busket_link.click()
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Button 'Add to basket' is not presented"
+        assert self.is_element_present(*ProductPageLocators.ADD_REWIEW_BUTTON), "Button 'Add rewiew' is not presented"
+        assert self.is_element_present(*ProductPageLocators.INSTOCK_AVAILABILITY), "Info 'Instock availability' is not presented"
+        assert PRODUCT_NAME in PRODUCT_ADDED_TO_BASKET, "Message about adding to basket ("+PRODUCT_ADDED_TO_BASKET+") does not contain product name ("+PRODUCT_NAME+")"
